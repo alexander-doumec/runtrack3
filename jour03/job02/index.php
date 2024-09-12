be7f837a8c205b6a5380badaae003f9b51caa02c
alexander-doumec <alexander.doumec@laplateforme.io>
2024-09-12 11:53:57 +0200

    jour03 update

--- jour03/job02/index.php
+++ jour03/job02/index.php
@@ -1,19 +1,18 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> <!-- Inclure jQuery -->
+    <title>Puzzle de l'arc-en-ciel</title>
     <link rel="stylesheet" href="style.css">
-    <title>Arc-en-ciel</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 <body>
-    <h1>Constituer l'Arc-en-ciel</h1>
-    <button id="melanger">Mélanger les images</button>
-    <div id="container" class="container"></div>
+    <h1>Reconstituez l'arc-en-ciel</h1>
+    <button id="melangerBouton">Mélanger les images</button>
+    <div id="container"></div>
+    <button id="verifierBouton">Vérifier l'ordre</button>
     <p id="message"></p>
-
     <script src="script.js"></script>
-    
 </body>
 </html>
--- jour03/job02/script.js
+++ jour03/job02/script.js
@@ -1,53 +1,63 @@
-$(document).ready(function () {
+$(document).ready(function() {
     const images = [
         'arc1.png',
         'arc2.png',
         'arc3.png',
         'arc4.png',
         'arc5.png',
-        'arc-.png'
+        'arc6.png'
     ];
 
-    //fonction pour afficher les images
-    function afficherImages() {
-        $('container').empty(); //Vider le conteneur avant d'ajouter une image
-        images.forEach((src, index) => {
-            const img = $('<img>').attr('src', src).addClass('image').attr('data-index', index);
-            $('#container').append(img);
+    function loadImages() {
+        $('#container').empty();
+        images.forEach(image => {
+            $('#container').append(`<img src="${image}" data-id="${image}" width="100" height="100">`);
+        });
+    }
 
+    function shuffle(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+    }
+
+    function enableDragAndDrop() {
+        $('#container img').draggable({
+            revert: "invalid"
         });
 
+        $('#container').droppable({
+            accept: "#container img",
+            drop: function(event, ui) {
+                $(ui.draggable).appendTo(this);
+                $(ui.draggable).css({top: 0, left: 0});
+            }
+        });
     }
-    $('#melanger').click(function () {
-        images.sort(() => Math.random() - 0.5); //Mélanger les images
-        afficherImages();//Afficher les images mélanger
-        $('#message').text('')
+
+    $('#melangerBouton').click(function() {
+        shuffle(images);
+        loadImages();
+        enableDragAndDrop();
+        $('#message').text('').css('color', '');
     });
 
-    //Vérifier l'ordre des images
-    $('container').on('click', 'image', function () {
-        const index = $(this).data('index');
-        const currentImg = images[index];
-        const nextIndex = (index + 1) % images.length //obtenir l'index de l'image suivante
-
-        //Echanger l'image actuelle avec l'image actuelle avec l'image suivante 
-        images[index] = images[nextIndex];
-        images[nextIndex] = currentImg;
-
-        afficherImages(); // Afficher les images mises à jour
-        verifierOrdre(); // Vérifier si les images sont dans le bon ordre
-    });
-
-    // Vérifier si les images sont dans le bon ordre
-    function verifierOrdre() {
-        const estCorrect = images.every((img, index) => img === `arc${index + 1}.png`);
-        if (estCorrect) {
-            $('#message').text('Vous avez gagné !').css('color', 'green');
+    $('#verifierBouton').click(function() {
+        let correctOrder = true;
+        $('#container img').each(function(index) {
+            if ($(this).data('id') !== images[index]) {
+                correctOrder = false;
+            }
+        });
+        if (correctOrder) {
+            $('#message').text('Vous avez gagné').css('color', 'green');
         } else {
-            $('#message').text('Vous avez perdu !').css('color', 'red');
+            $('#message').text('Vous avez perdu').css('color', 'red');
         }
-    }
+    });
 
-    // Afficher les images initialement
-    afficherImages();
+    loadImages();
+    enableDragAndDrop();
 });

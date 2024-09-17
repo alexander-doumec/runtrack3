7c60df1fbff54ef5e76d5290d0df3fedc53c10e2
alexander-doumec <alexander.doumec@laplateforme.io>
2024-09-17 09:52:19 +0200

    update

--- jour04/job04/index.php
+++ jour04/job04/index.php
@@ -5,30 +5,30 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Liste des utilisateurs</title>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
 </head>
 
 <body>
   <h1>Liste des utilisateurs</h1>
-  <table id="userTable">
+  <table id="userTable" border="1">
     <thead>
       <tr>
         <th>ID</th>
         <th>Nom</th>
         <th>Prénom</th>
         <th>Email</th>
-        <th>Mettre à jour</th>
       </tr>
     </thead>
-    <tbody id="'userList">
-      <!-- Les données seront insérés ici -->
+
+    <tbody>
+      <!-- Les utilisateurs seront insérés ici via AJAX -->
     </tbody>
   </table>
 
-  <!-- Bouton pour rafraichir la Liste -->
-   <button onclick="rafraîchirListe">Mettre à jour </button>
-
-
   <script src="script.js"></script>
+  
+  <button id="updateButton">Update</button>
 </body>
 
 </html>
